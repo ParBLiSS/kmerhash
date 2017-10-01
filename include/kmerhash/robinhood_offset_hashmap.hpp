1e6b435d96154216042840593cf2ff01ee7b2be4
Sanchit Misra <sanchit.misra@intel.com>
2017-10-01 06:29:08 -0700

    Getting rid of compilation error complaining about the arguments to _mm_prefetch

--- include/kmerhash/robinhood_offset_hashmap.hpp
+++ include/kmerhash/robinhood_offset_hashmap.hpp
@@ -64,7 +64,7 @@
 // should be easier for prefetching
 #if ENABLE_PREFETCH
 #include "xmmintrin.h" // prefetch related.
-#define KH_PREFETCH(ptr, level)  _mm_prefetch(ptr, level)
+#define KH_PREFETCH(ptr, level)  _mm_prefetch(reinterpret_cast<const char*>(ptr), level)
 #else
 #define KH_PREFETCH(ptr, level)
 #endif
