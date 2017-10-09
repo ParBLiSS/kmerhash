60e62d745fcf8e429b1a36b038b886fb9aa00976
Tony Pan <tpan7@gatech.edu>
2017-10-08 22:20:54 -0400

    ADD: fast gcd and lcm.  not yet used...

--- include/kmerhash/math_utils.hpp
+++ include/kmerhash/math_utils.hpp
@@ -24,6 +24,8 @@
 #ifndef KMERHASH_MATH_UTILS_HPP_
 #define KMERHASH_MATH_UTILS_HPP_
 
+#include <numeric>
+
 #if defined(__INTEL_COMPILER)
 #define CONSTEXPR
 #else
@@ -59,12 +61,20 @@
 
 #include <x86intrin.h>
 
-template <typename T>
-inline CONSTEXPR T next_power_of_2(T x) {
+inline uint64_t next_power_of_2(uint64_t x) {
 
-  static_assert(::std::is_integral<T>::value && !::std::is_signed<T>::value,
-                "ERROR: can only find power of 2 for unsigned integers.");
-  return  0x1ULL << (64 - _lzcnt_u64(x-1));
+//  static_assert(::std::is_integral<T>::value && !::std::is_signed<T>::value,
+//                "ERROR: can only find power of 2 for unsigned integers.");
+  return  0x1ULL << (64 - __lzcnt64(x-1));
+}
+
+#elif defined(__GNUC__)
+
+inline uint64_t next_power_of_2(uint64_t x) {
+
+//  static_assert(::std::is_integral<T>::value && !::std::is_signed<T>::value,
+//                "ERROR: can only find power of 2 for unsigned integers.");
+  return  0x1ULL << (64 - __builtin_lzcll(x-1));
 }
 
 #else
@@ -130,5 +140,164 @@ inline CONSTEXPR T next_power_of_2(T x) {
 
 #endif
 
+
+
+// compute gcd
+#include <numeric>
+
+#if defined(__BMI__)
+
+// use built in.  from  https://hbfs.wordpress.com/2013/12/10/the-speed-of-gcd/
+template <typename T,
+ 	 typename ::std::enable_if<(sizeof(T) > 4), int>::type = 1>
+T gcd(T a, T b)
+{
+  if (a == 0) return b;
+  if (b == 0) return a;
+
+  int shift = __tzcnt_u64(a|b);
+  a >>= __tzcnt_u64(a);
+
+  do
+   {
+    b>>=__tzcnt_u64(b);
+
+    if (a>b) std::swap(a,b);
+    b-=a;
+   } while (b);
+
+  return a << shift;
+}
+
+template <typename T,
+ 	 typename ::std::enable_if<(sizeof(T) <= 4) && (sizeof(T) > 2), int>::type = 1>
+T gcd(T a, T b)
+{
+  if (a == 0) return b;
+  if (b == 0) return a;
+
+  int shift = __tzcnt_u32(a|b);
+  a >>= __tzcnt_u32(a);
+
+  do
+   {
+    b>>=__tzcnt_u32(b);
+
+    if (a>b) std::swap(a,b);
+    b-=a;
+   } while (b);
+
+  return a << shift;
+}
+
+template <typename T,
+ 	 typename ::std::enable_if<(sizeof(T) <= 2), int>::type = 1>
+T gcd(T a, T b)
+{
+  if (a == 0) return b;
+  if (b == 0) return a;
+
+  int shift = __tzcnt_u16(a|b);
+  a >>= __tzcnt_u16(a);
+
+  do
+   {
+    b>>=__tzcnt_u16(b);
+
+    if (a>b) std::swap(a,b);
+    b-=a;
+   } while (b);
+
+  return a << shift;
+}
+
+#elif defined(__GNU__)
+// use built in.  from  https://hbfs.wordpress.com/2013/12/10/the-speed-of-gcd/
+template <typename T,
+ 	 typename ::std::enable_if<(sizeof(T) > 4), int>::type = 1>
+T gcd(T a, T b)
+{
+  if (a == 0) return b;
+  if (b == 0) return a;
+
+  int shift = __builtin_ctzll(a|b);
+  a >>= __builtin_ctzll(a);
+
+  do
+   {
+    b>>=__builtin_ctzll(b);
+
+    if (a>b) std::swap(a,b);
+    b-=a;
+   } while (b);
+
+  return a << shift;
+}
+
+template <typename T,
+ typename ::std::enable_if<(sizeof(T) <= 4) && (sizeof(T) > 2), int>::type = 1>
+T gcd(T a, T b)
+{
+  if (a == 0) return b;
+  if (b == 0) return a;
+
+  int shift = __builtin_ctz(a|b);
+  a >>= __builtin_ctz(a);
+
+  do
+   {
+    b>>=__builtin_ctz(b);
+
+    if (a>b) std::swap(a,b);
+    b-=a;
+   } while (b);
+
+  return a << shift;
+}
+
+template <typename T,
+  typename ::std::enable_if<(sizeof(T) <= 2), int>::type = 1>
+T gcd(T a, T b)
+{
+  if (a == 0) return b;
+  if (b == 0) return a;
+
+  int shift = __builtin_ctzs(a|b);
+  a >>= __builtin_ctzs(a);
+
+  do
+   {
+    b>>=__builtin_ctzs(b);
+
+    if (a>b) std::swap(a,b);
+    b-=a;
+   } while (b);
+
+  return a << shift;
+}
+
+
+#else
+// no built in, so use iterative + mod.  from https://hbfs.wordpress.com/2013/12/10/the-speed-of-gcd/
+T gcd(T a, T b)
+{
+	T t;
+	while (b)
+	{
+		t=b;
+		b=a % b;
+		a=t;
+	}
+	return a;
+}
+#endif
+
+template <typename T>
+T lcm(T a, T b)
+{
+    T temp = gcd(a, b);
+
+    return temp ? ((a / temp) * b) : 0;
+}
 
 #endif /* KMERHASH_MATH_UTILS_HPP_ */
